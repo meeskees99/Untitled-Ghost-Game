d9b19a8f04a93d57e474b756e0025c0d9d95d18c
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-05-15 16:07:27 +0200

    syncvar

--- Assets/Scripts/TeamData.cs
+++ Assets/Scripts/TeamData.cs
@@ -1,9 +1,11 @@
+using FishNet.Object.Synchronizing;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TeamData : MonoBehaviour
 {
+    [SyncVar]
     public List<PlayerData> tData = new List<PlayerData>();
     public int teamIndex;
 }
