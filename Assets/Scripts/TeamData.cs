bd935ffe7316ff924c532a56d79e6835a2d3bf06
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-05-15 16:08:01 +0200

    networkbehaviour

--- Assets/Scripts/TeamData.cs
+++ Assets/Scripts/TeamData.cs
@@ -1,9 +1,10 @@
+using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeamData : MonoBehaviour
+public class TeamData : NetworkBehaviour
 {
     [SyncVar]
     public List<PlayerData> tData = new List<PlayerData>();
