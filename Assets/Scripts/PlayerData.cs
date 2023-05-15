5bc7082d9f19ac6daa05b598aadcc499b9b020c9
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-05-15 16:45:53 +0200

    id

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -24,6 +24,7 @@ public class PlayerData : NetworkBehaviour
         }
         if (playerId == -2)
         {
+            print("ID " + InstanceFinder.ClientManager.Connection.ClientId);
             SetPlayerID(InstanceFinder.ClientManager.Connection.ClientId);
         }
         
