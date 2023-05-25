2d3f5f90c3ec998284928fc8db7d222bbebb1083
Gerrielofo <gerlof.reinink@gmail.com>
2023-05-25 16:24:45 +0200

    audio percent

--- Assets/UXUI/Scripts/Mixer.cs
+++ Assets/UXUI/Scripts/Mixer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using TMPro;
@@ -11,22 +9,21 @@ public class Mixer : MonoBehaviour
     [SerializeField] TMP_Text musicPerc;
     [SerializeField] TMP_Text sfxPerc;
 
-    // Zet de sliders van -60 tot 20
     public void SetMasterVol(float masterLvl)
     {
-        masterMixer.SetFloat("MasterVol", Mathf.Log10(masterLvl)*20);
-        masterPerc.text = (masterLvl * 1.25 + 75).ToString("0") + "%";
+        masterMixer.SetFloat("MasterVol", Mathf.Log10(masterLvl) * 20);
+        masterPerc.text = (masterLvl * 100).ToString("0") + "%";
     }
 
     public void SetSFXVol(float sfxLvl)
     {
         masterMixer.SetFloat("SFXVol", Mathf.Log10(sfxLvl) * 20);
-        sfxPerc.text = (sfxLvl * 1.25 + 75).ToString("0") + "%";
+        sfxPerc.text = (sfxLvl * 100).ToString("0") + "%";
     }
 
-    public void SetMusicVol(float musicVol)
+    public void SetMusicVol(float musicLvl)
     { 
-        masterMixer.SetFloat("MusicVol", Mathf.Log10(musicVol) * 20);
-        musicPerc.text = (musicVol * 1.25 + 75).ToString("0") + "%";
+        masterMixer.SetFloat("MusicVol", Mathf.Log10(musicLvl) * 20);
+        musicPerc.text = (musicLvl * 100).ToString("0") + "%";
     }
 }
