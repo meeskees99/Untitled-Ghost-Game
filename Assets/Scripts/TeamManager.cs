f83c5f73b77ee60a2018d3278852432ff7f1d256
Gerrielofo <gerlof.reinink@gmail.com>
2023-06-01 15:57:13 +0200

    Update TeamManager.cs

--- Assets/Scripts/TeamManager.cs
+++ Assets/Scripts/TeamManager.cs
@@ -143,7 +143,7 @@ public class TeamManager : NetworkBehaviour
         player.GetComponent<PlayerData>().UI.transform.SetParent(rects[team].transform);
         ClearUiPlayers();
         ClearTeamStart();
-        for (int z = 0; z < currentClients -1; z++)
+        for (int z = 0; z < currentClients; z++)
         {
             SetTeamStart(players[z]);
             SetPlayers(players[z]);
