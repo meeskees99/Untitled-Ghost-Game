e51bcec29c9f810068241aa8a6091981a30e4aac
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-06-05 13:29:57 +0200

    spawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class GameManager : NetworkBehaviour
 {
@@ -10,22 +11,48 @@ public class GameManager : NetworkBehaviour
     int GhostIndex;
     [SerializeField] Transform spawnLocation;
 
+    [SerializeField] PlayerData[] players;
+
+    [SerializeField] Transform[] team1points;
+    [SerializeField] Transform[] team2points;
+
+    GameObject ghost;
+
+    int team1Index;
+    int team2Index;
     // Start is called before the first frame update
     void Start()
     {
-        SpawnAgent();
+        if (IsHost && ghost == null)
+        {
+            SpawnAgent();
+            players = FindObjectsOfType<PlayerData>();
+        }
+        team1Index= 0;
+        team2Index = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetTeamPoints()
     {
-        
+        for (int i = 0; i < players.Length; i++)
+        {
+            if(players[i].teamID == 0)
+            {
+                players[i].transform.position = team1points[team1Index].transform.position;
+                team1Index++;
+            }
+            else if (players[i].teamID == 1)
+            {
+                players[i].transform.position = team1points[team2Index].transform.position;
+                team2Index++;
+            }
+        }
     }
     
     [ServerRpc(RequireOwnership = false)]
     void SpawnAgent()
     {
-        GameObject ghost = Instantiate(ghosts[GhostIndex], spawnLocation.position, spawnLocation.rotation);
+        ghost = Instantiate(ghosts[GhostIndex], spawnLocation.position, spawnLocation.rotation);
         Spawn(ghost);
     }
 }
