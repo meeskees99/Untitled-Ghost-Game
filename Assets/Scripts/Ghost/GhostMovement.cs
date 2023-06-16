4f9dfb0548f690b28dc14ebc3597d68c6e1be366
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-06-16 15:46:44 +0200

    Update GhostMovement.cs

--- Assets/Scripts/Ghost/GhostMovement.cs
+++ Assets/Scripts/Ghost/GhostMovement.cs
@@ -32,7 +32,7 @@ public class GhostMovement : NetworkBehaviour
 
     [SyncVar] public bool isDead;
 
-    [SyncVar] public bool hitnessess;
+    [SyncVar] [SerializeField] public bool hitnessess;
     // Start is called before the first frame update
     void Start()
     {
