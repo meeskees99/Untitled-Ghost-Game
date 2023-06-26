fd0f9e880e7aaf480b934dbe1b0f9b5c5e3687a2
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-06-26 16:58:42 +0200

    Update MouseLookAdvanced.cs

--- Assets/Scripts/MouseLookAdvanced.cs
+++ Assets/Scripts/MouseLookAdvanced.cs
@@ -62,6 +62,7 @@ public class MouseLookAdvanced : NetworkBehaviour
                         if (hit.transform.GetComponent<MovementAdvanced>().IsStunned)
                         {
                             print("isStunned");
+                            print(hit.transform.GetChild(1).GetChild(0));
                             stofZuiger.StealPoints(hit.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<StofZuiger>().GhostPoints, hit.transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<StofZuiger>());
                         }
                     }
@@ -81,7 +82,7 @@ public class MouseLookAdvanced : NetworkBehaviour
                         if (hit.transform.GetComponent<MovementAdvanced>().IsStunned)
                         {
                             print("isStunned");
-                            print(hit.transform.GetChild(1));
+                            print(hit.transform.GetChild(1).GetChild(0));
                             stofZuiger.StealPoints(hit.transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<StofZuiger>().GhostPoints, hit.transform.GetChild(2).GetChild(0).GetChild(0).GetComponent<StofZuiger>());
                         }
                     }
