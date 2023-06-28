539d17722bc8c671327584182d33561a435132c1
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-06-28 14:30:11 +0200

    Revert "Update MovementAdvanced.cs"
    This reverts commit 80b87b9768c29f341a5869975778dd7f645b5828.

--- Assets/Scripts/Movement/MovementAdvanced.cs
+++ Assets/Scripts/Movement/MovementAdvanced.cs
@@ -100,21 +100,19 @@ public class MovementAdvanced : NetworkBehaviour
         characterIndex = charint;
         charSet = true;
         CharIntObserver(charint);
-        print(charSet + " int set server");
+
     }
     [SyncVar][SerializeField] bool charSet;
     [ServerRpc(RequireOwnership = false)]
     void CharSet(bool value)
     {
         charSet = value;
-        print(charSet + " character set server");
     }
     [ObserversRpc]
     void CharIntObserver(int charint)
     {
         characterIndex = charint;
         charSet = true;
-        print(charSet + " character set observer");
     }
     // [ServerRpc(RequireOwnership = false)]
     // void DoTHing()
@@ -125,7 +123,7 @@ public class MovementAdvanced : NetworkBehaviour
 
     private void Update()
     {
-        print(charSet + " character set update");
+        print(charSet + " character set");
         if (charSet)
         {
             // DoTHing();
