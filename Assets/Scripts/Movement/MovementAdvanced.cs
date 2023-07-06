3cd3d84ee2c80f2a1497fc46f45d4380576aac9e
Gerrielofo <gerlof.reinink@gmail.com>
2023-07-06 14:15:08 +0200

    Light add redo

--- Assets/Scripts/Movement/MovementAdvanced.cs
+++ Assets/Scripts/Movement/MovementAdvanced.cs
@@ -40,6 +40,8 @@ public class MovementAdvanced : NetworkBehaviour
     [Header("Character")]
     public GameObject[] character;
     [SyncVar] public int characterIndex;
+    public List<GameObject> gunLights = new();
+    public List<GameObject> tankLights = new();
 
     [SyncVar] bool canSteal;
 
@@ -155,6 +157,15 @@ public class MovementAdvanced : NetworkBehaviour
         }
         animator = character[index].GetComponent<Animator>();
         stofZuiger.animator = character[index].GetComponent<Animator>();
+
+        gunLights.Add(character[index].transform.GetChild(1).gameObject);
+        gunLights.Add(character[index].transform.GetChild(2).gameObject);
+        gunLights.Add(character[index].transform.GetChild(3).gameObject);
+
+        tankLights.Add(character[index].transform.GetChild(5).gameObject);
+        tankLights.Add(character[index].transform.GetChild(6).gameObject);
+        tankLights.Add(character[index].transform.GetChild(7).gameObject);
+        SetCharObserver(index);
     }
 
 
@@ -185,6 +196,8 @@ public class MovementAdvanced : NetworkBehaviour
         {
             rb.drag = 0f;
         }
+
+        SetLightValue(stofZuiger.GhostPoints);
     }
 
     private void FixedUpdate()
@@ -416,4 +429,18 @@ public class MovementAdvanced : NetworkBehaviour
         animator.SetBool(s, b);
     }
 
+    [ServerRpc(RequireOwnership = false)]
+    public void SetLightValue(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            gunLights[i].SetActive(true);
+            tankLights[i].SetActive(true);
+        }
+        for (int y = 2; y > value - 1; y--)
+        {
+            gunLights[y].SetActive(false);
+            tankLights[y].SetActive(false);
+        }
+    }
 }
